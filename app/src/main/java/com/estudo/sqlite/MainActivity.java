412646fe1004683f61d90bf3ef3485b05e54e0db
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-11-16 19:12:54 -0300

    Códigos para criar filtros personalidos

--- app/src/main/java/com/estudo/sqlite/MainActivity.java
+++ app/src/main/java/com/estudo/sqlite/MainActivity.java
@@ -23,16 +23,49 @@ public class MainActivity extends AppCompatActivity {
             // inserir dados na tabela
             database.execSQL("INSERT INTO persons(name, age) VALUES('João', 31)");
             database.execSQL("INSERT INTO persons(name, age) VALUES('Maria', 32)");
+            database.execSQL("INSERT INTO persons(name, age) VALUES('Lea', 18)");
+            database.execSQL("INSERT INTO persons(name, age) VALUES('José', 50)");
+            database.execSQL("INSERT INTO persons(name, age) VALUES('Mario', 40)");
+
             // Recuperar dados
-            Cursor cursor = database.rawQuery("SELECT name, age FROM persons", null);
+//            String query =
+//                    "SELECT name, age FROM persons " +
+//                            "WHERE name = 'João'";
+
+//            String query =
+//                    "SELECT name, age FROM persons " +
+//                            "WHERE age >= 35 OR age = 18";
+
+//            String query =
+//                    "SELECT name, age FROM persons " +
+//                            "WHERE age IN(18,35) ";
+
+//            String query =
+//                    "SELECT name, age FROM persons " +
+//                            "WHERE age BETWEEN 30 AND 35";
+
+//            String query =
+//                    "SELECT name, age FROM persons " +
+//                            "WHERE name LIKE 'José'";
+
+            // ASC ascendente ou DESC descendente
+            // LIMIT 3
+            String query =
+                    "SELECT name, age FROM persons " +
+                            "WHERE 1=1 ORDER BY age ASC";
+
+            Cursor cursor = database.rawQuery(query, null);
+
             // Indices da tabela
             int indexName = cursor.getColumnIndex("name");
             int indexAge = cursor.getColumnIndex("age");
 
             cursor.moveToFirst();
             while (cursor != null) {
-                Log.i("Result - name: ", cursor.getString(indexName));
-                Log.i("Result - age: ", cursor.getString(indexAge));
+                String name = cursor.getString(indexName);
+                String age = cursor.getString(indexAge);
+
+                Log.i("Result - name: ", name + "Result - age: " + age);
                 cursor.moveToNext();
             }
 
