cfaa44ccc38625ca5c82e990f805fd80de6daa37
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-11-17 19:18:14 -0300

    Adiciona comandos para adicionar ou remover registros de tabelas, altera ícone do app

--- app/src/main/java/com/estudo/sqlite/MainActivity.java
+++ app/src/main/java/com/estudo/sqlite/MainActivity.java
@@ -19,13 +19,26 @@ public class MainActivity extends AppCompatActivity {
             // Cria banco de dados
             SQLiteDatabase database = openOrCreateDatabase("app", MODE_PRIVATE, null);
             // Cria tabela do BD
-            database.execSQL("CREATE TABLE IF NOT EXISTS persons(name VARCHAR, age INT(3))");
+            database.execSQL("CREATE TABLE IF NOT EXISTS persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, age INT(3))");
+
+            //Deletar tabela
+            //database.execSQL("DROP TABLE persons");
+
             // inserir dados na tabela
-            database.execSQL("INSERT INTO persons(name, age) VALUES('João', 31)");
-            database.execSQL("INSERT INTO persons(name, age) VALUES('Maria', 32)");
-            database.execSQL("INSERT INTO persons(name, age) VALUES('Lea', 18)");
-            database.execSQL("INSERT INTO persons(name, age) VALUES('José', 50)");
-            database.execSQL("INSERT INTO persons(name, age) VALUES('Mario', 40)");
+//            database.execSQL("INSERT INTO persons(name, age) VALUES('João', 31)");
+//            database.execSQL("INSERT INTO persons(name, age) VALUES('Maria', 32)");
+//            database.execSQL("INSERT INTO persons(name, age) VALUES('Lea', 18)");
+//            database.execSQL("INSERT INTO persons(name, age) VALUES('José', 50)");
+//            database.execSQL("INSERT INTO persons(name, age) VALUES('Mario', 40)");
+
+            // Atualiza registros
+            //database.execSQL("UPDATE persons SET age = 19 WHERE name = 'Lea'");
+            //database.execSQL("UPDATE persons SET age = 19, name = 'Lea Dominguez' WHERE id = 3");
+
+            // Deletando um registro da tabela
+            //database.execSQL("DELETE FROM persons WHERE id = 10");
+            // Apagar todos os registros da tabela
+            database.execSQL("DELETE FROM persons");
 
             // Recuperar dados
 //            String query =
@@ -50,22 +63,28 @@ public class MainActivity extends AppCompatActivity {
 
             // ASC ascendente ou DESC descendente
             // LIMIT 3
+//            String query =
+//                    "SELECT name, age FROM persons " +
+//                            "WHERE 1=1 ORDER BY age";
+
             String query =
-                    "SELECT name, age FROM persons " +
-                            "WHERE 1=1 ORDER BY age ASC";
+                    "SELECT * FROM persons " +
+                            "WHERE 1=1";
 
             Cursor cursor = database.rawQuery(query, null);
 
             // Indices da tabela
+            int indexID = cursor.getColumnIndex("id");
             int indexName = cursor.getColumnIndex("name");
             int indexAge = cursor.getColumnIndex("age");
 
             cursor.moveToFirst();
             while (cursor != null) {
+                String id = cursor.getString(indexID);
                 String name = cursor.getString(indexName);
                 String age = cursor.getString(indexAge);
 
-                Log.i("Result - name: ", name + "Result - age: " + age);
+                Log.i("Result - name ", name + " " + "Age: " + age + " " + "ID: " + id);
                 cursor.moveToNext();
             }
 
